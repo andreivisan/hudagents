cd5b42f6530ffa2b136c76f95b938447d37aabd1
Andrei Visan <andrei.m.visan@gmail.com>
2025-11-24 14:03:18 +0100

    Capture CLI: Basic command

--- crates/hudagents-capture/src/main.rs
+++ crates/hudagents-capture/src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+const DEFAULT_DURATION_SECS: u64 = 10;
+const DEFAULT_OUTPUT_DIR: &str = "../assets";
+
+#[derive(Parser)]
+struct Args{
+    #[arg(short, long)]
+    session: String,
+    #[arg(short, long, default_value_t = DEFAULT_DURATION_SECS)]
+    duration: u64,
+    #[arg(short, long, default_value = DEFAULT_OUTPUT_DIR)]
+    output_dir: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Session: {}", args.session);
+    println!("Duration: {} seconds", args.duration);
+    println!("Output directory: {}", args.output_dir.display());
 }
