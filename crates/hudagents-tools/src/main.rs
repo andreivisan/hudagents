71cfad5d902dff7bb9a27262bbe6a9e8d7f34bb0
Andrei Visan <andrei.m.visan@gmail.com>
2025-12-01 11:36:38 +0100

    Tools: Started work on sysinfo

--- crates/hudagents-tools/src/main.rs
+++ crates/hudagents-tools/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use whisper_rs::print_system_info;
 
 #[derive(Parser)]
 #[command {name = "hudagents-tools"}]
@@ -18,11 +19,17 @@ enum Commands {
     }
 }
 
+// TODO: Use a DEBUG_LEVEL flag to control the verbosity of the output
+fn sysinfo() {
+    println!("--- Whisper System Info ---");
+    println!("{}", print_system_info());
+}
+
 fn main() {
     let cli = Cli::parse();
     match cli.command {
         Commands::Sysinfo => {
-            println!("Running system info...");
+            sysinfo();
         }   
         Commands::Download { model, path } => {
             println!("Downloading model {} to {}", model, path.unwrap_or_default());
