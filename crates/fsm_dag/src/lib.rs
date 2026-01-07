920a5198b328ce8c5ac1010361d693bfc3bbf15f
Andrei Visan <andrei.m.visan@gmail.com>
2026-01-07 13:26:48 +0100

    HA Practice: Finite State Machine

--- crates/fsm_dag/src/lib.rs
+++ crates/fsm_dag/src/lib.rs
@@ -7,13 +7,37 @@ enum Event { Coin, Push }
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Action { Unlock, Lock, Alarm, ThankYou, NoOp }
 
-fn step(state: State, event: Event) -> (State, Action) {
-    match (state, event) {
-        (State::Locked, Event::Coin) => (State::Unlocked, Action::Unlock),
-        (State::Locked, Event::Push) => (State::Locked, Action::Alarm),
-        (State::Unlocked, Event::Coin) => (State::Unlocked, Action::ThankYou),
-        (State::Unlocked, Event::Push) => (State::Locked, Action::Lock)
-    }
+#[derive(Debug, Default)]
+struct Ctx {
+    coins: u32,
+    pushes: u32,
+    alarms: u32,
+}
+
+fn step(state: State, event: Event, ctx: &mut Ctx) -> (State, Vec<Action>) {
+    let mut actions = Vec::new();
+    let state: State  = match (state, event) {
+        (State::Locked, Event::Coin) => {
+            ctx.coins += 1;
+            actions.push(Action::Unlock);
+            State::Unlocked
+        },
+        (State::Locked, Event::Push) => {
+            ctx.alarms += 1;
+            return (State::Locked, vec![Action::Alarm])
+        },
+        (State::Unlocked, Event::Coin) => {
+            ctx.coins += 1;
+            actions.push(Action::ThankYou);
+            State::Unlocked
+        },
+        (State::Unlocked, Event::Push) => {
+            ctx.pushes += 1;
+            actions.push(Action::Lock);
+            State::Locked
+        }
+    };
+    (state, actions)
 }
 
 #[cfg(test)]
@@ -21,31 +45,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_locked_coin() {
-        let (state, action) = step(State::Locked, Event::Coin);
-        assert_eq!(State::Unlocked, state);
-        assert_eq!(Action::Unlock, action);
-    }    
-
-    #[test]
-    fn test_locked_push() {
-        let (state, action) = step(State::Locked, Event::Push);
-        assert_eq!(State::Locked, state);
-        assert_eq!(Action::Alarm, action);
+    fn test_multiple_events() {
+        // Create a queue of events
+        // Loop through the queue
+        // Append actions for each event to an outer accumulator
     }
 
-    #[test]
-    fn test_unlocked_coin() {
-        let (state, action) = step(State::Unlocked, Event::Coin);
-        assert_eq!(State::Unlocked, state);
-        assert_eq!(Action::ThankYou, action);
-    }
-
-    #[test]
-    fn test_unlocked_push() {
-        let (state, action) = step(State::Unlocked, Event::Push);
-        assert_eq!(State::Locked, state);
-        assert_eq!(Action::Lock, action);
-    }
 }
 
