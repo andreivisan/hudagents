3e48b139c3937cca003b363fc0ef287256601f69
Andrei Visan <andrei.m.visan@gmail.com>
2026-01-08 17:47:45 +0100

    HA Practice: Finite State Machine - Temp progress

--- crates/fsm_dag/src/lib.rs
+++ crates/fsm_dag/src/lib.rs
@@ -5,9 +5,9 @@ enum State { Locked, Unlocked }
 enum Event { Coin, Push }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Action { Unlock, Lock, Alarm, ThankYou, NoOp }
+enum Action { Unlock, Lock, Alarm, ThankYou, EnqueueCoin, EnqueuePush}
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq, Eq)]
 struct Ctx {
     coins: u32,
     pushes: u32,
@@ -24,6 +24,7 @@ fn step(state: State, event: Event, ctx: &mut Ctx) -> (State, Vec<Action>) {
         },
         (State::Locked, Event::Push) => {
             ctx.alarms += 1;
+            ctx.pushes += 1;
             return (State::Locked, vec![Action::Alarm])
         },
         (State::Unlocked, Event::Coin) => {
@@ -40,15 +41,29 @@ fn step(state: State, event: Event, ctx: &mut Ctx) -> (State, Vec<Action>) {
     (state, actions)
 }
 
+fn run(init_state: State, init_events: Vec<Event>, max_steps: usize) -> (State, Ctx, Vec<Action>) {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_multiple_events() {
-        // Create a queue of events
-        // Loop through the queue
-        // Append actions for each event to an outer accumulator
+        // Create a vec of events
+        let events = vec![Event::Push, Event::Coin, Event::Push, Event::Coin];
+        let mut state = State::Locked;
+        let mut actions: Vec<Action> = Vec::new();
+        let mut ctx: Ctx = Ctx::default();
+        // Loop through the vec
+        for event in events {
+            let (next_state, temp_acc) = step(state, event, &mut ctx);
+            // Append actions for each event to an outer accumulator
+            actions.extend(temp_acc);
+            state = next_state;
+        }
+        assert_eq!(ctx, Ctx{ coins: 2, pushes: 2, alarms: 1 });
+        assert_eq!(state, State::Unlocked);
+        assert_eq!(actions, vec![Action::Alarm, Action::Unlock, Action::Lock, Action::Unlock]);
     }
 
 }
