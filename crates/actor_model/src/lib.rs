b8891159a52e6830d1ba09ec324a680b89629805
Andrei Visan <andrei.m.visan@gmail.com>
2026-01-14 15:50:44 +0100

    Started the Actor Model

--- crates/actor_model/src/lib.rs
+++ crates/actor_model/src/lib.rs
@@ -1,7 +1,8 @@
+use std::future::Future;
 use tokio::{
     sync::{
-        oneshot::Sender,
-        mpsc::{Receiver, Sender as MpscSender}
+        mpsc::{channel, Sender},
+        oneshot::{self, Sender as ReplyTx},
     },
     task::JoinHandle
 };
@@ -12,72 +13,158 @@ pub enum ActorError {
     ResponseDropped,
 }
 
-enum Message {
-    Add { delta: i64, reply: Sender },
-    Get { reply: Sender<i64> },
+#[derive(Debug)]
+pub enum ActorCtrl {
+    Continue,
     Stop,
 }
 
-pub struct CounterActor {
-    count: i64,
-    rx: Receiver,
+#[derive(Debug)]
+pub enum ExitReason {
+    StoppedByMessage,
+    AllSendersDropped,
 }
 
-impl CounterActor {
-    async fn run(mut self) {
-        while let Some(msg) = self.rx.recv() await {
-            match msg {
-                M
-            }
-        }
-    }
+enum Message {
+    Add { delta: i64, reply: ReplyTx<i64> },
+    Get { reply: ReplyTx<i64> },
+    Stop { reply: ReplyTx<()> },
 }
 
 #[derive(Clone)]
 pub struct CounterHandle {
-    tx: MpscSender<Message>, 
+    tx: Sender<Message>, 
 }
 
-pub fn spawn_counter(counter: usize) -> (CounterHandle, JoinHandle<()>) {
-    let (tx, rx) = oneshot::channel();
+impl CounterHandle {
+    async fn request<T>(
+        &self, 
+        make_msg: impl FnOnce(ReplyTx<T>) -> Message
+    ) -> Result<T, ActorError> {
+        let (reply_tx,  reply_rx) = oneshot::channel();
+        let msg = make_msg(reply_tx);
+        self.tx.send(msg).await.map_err(|_| ActorError::SendFailed)?;
+        reply_rx.await.map_err(|_| ActorError::ResponseDropped)
+    } 
+
+    pub async fn add(&self, delta: i64) -> Result<i64, ActorError> {
+        self.request(|reply| Message::Add { delta, reply }).await
+    }
+
+    pub async fn get(&self) -> Result<i64, ActorError> {
+        self.request(|reply| Message::Get { reply }).await
+    }
+
+    pub async fn stop(&self) -> Result<(), ActorError> {
+        self.request(|reply| Message::Stop { reply }).await
+    }
+}
+
+pub fn spawn_actor<State, Msg, Handler, Fut>(
+    capacity: usize,
+    initial_state: State,
+    handler: Handler
+) -> (Sender<Msg>, JoinHandle<ExitReason>)
+where
+    State: Send + 'static,
+    Msg: Send + 'static,
+    Handler: FnMut(&mut State, Msg) -> Fut + Send + 'static,
+    Fut: Future<Output = ActorCtrl> + Send + 'static
+{
+    let (tx, mut rx) = channel::<Msg>(capacity);
+    let join = tokio::spawn(async move {
+        let mut state = initial_state;
+        let mut handler = handler;
+        while let Some(msg) = rx.recv().await {
+            match handler(&mut state, msg).await {
+                ActorCtrl::Continue => {}
+                ActorCtrl::Stop => return ExitReason::StoppedByMessage,
+            }
+        }
+        ExitReason::AllSendersDropped
+    });
+    (tx, join)
 }
 
 
-// use tokio::sync::mpsc;
-//
-//   struct CounterActor {
-//       count: i64,
-//       rx: mpsc::Receiver<Msg>,
-//   }
-//
-//   impl CounterActor {
-//       async fn run(mut self) {
-//           while let Some(msg) = self.rx.recv().await {
-//               match msg {
-//                   Msg::Add { delta, reply } => {
-//                       self.count += delta;
-//                       let _ = reply.send(self.count);
-//                   }
-//                   Msg::Get { reply } => {
-//                       let _ = reply.send(self.count);
-//                   }
-//                   Msg::Stop { reply } => {
-//                       let _ = reply.send(());
-//                       break;
-//                   }
-//               }
-//           }
-//       }
-//   }
-//
-//   And the spawn flow (still just a sketch):
-//
-//   pub fn spawn_counter(capacity: usize) -> (CounterHandle, JoinHandle<()>) {
-//       let (tx, rx) = mpsc::channel::<Msg>(capacity);
-//       let actor = CounterActor { count: 0, rx };
-//
-//       let join = tokio::spawn(actor.run());
-//       let handle = CounterHandle { tx };
-//
-//       (handle, join)
-//   }
+pub fn spawn_counter(capacity: usize) -> (CounterHandle, JoinHandle<ExitReason>) {
+    let (tx, join) = spawn_actor(
+        capacity,
+        0_i64,
+        |state, msg| {
+            let ctrl = match msg {
+                Message::Add { delta, reply } => {
+                    *state += delta;
+                    let _ = reply.send(*state);
+                    ActorCtrl::Continue
+                }
+                Message::Get { reply } => {
+                    let _ = reply.send(*state);
+                    ActorCtrl::Continue
+                }
+                Message::Stop { reply } => {
+                    let _ = reply.send(());
+                    ActorCtrl::Stop
+                }
+            };
+
+            async move { ctrl }
+        },
+    );
+    (CounterHandle { tx }, join)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_counter_add_get_happy_path() {
+        let (handle, _join) = spawn_counter(8);
+
+        let v1 = handle.add(5).await.unwrap();
+        assert_eq!(v1, 5);
+
+        let v2 = handle.add(-2).await.unwrap();
+        assert_eq!(v2, 3);
+
+        let v3 = handle.get().await.unwrap();
+        assert_eq!(v3, 3);
+
+        let _ = handle.stop().await;
+    }
+
+    #[tokio::test]
+    async fn test_counter_stop_joins() {
+        let (handle, join) = spawn_counter(8);
+        
+        let stop_res = handle.stop().await;
+        assert!(stop_res.is_ok());
+
+        let join_res = join.await;
+        assert!(join_res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_counter_handle_clone_works() {
+        let (handle, _join) = spawn_counter(8);
+        let handle2 = handle.clone();
+
+        let v1 = handle.add(2).await.unwrap();
+        assert_eq!(v1, 2);
+
+        let v2 = handle2.add(3).await.unwrap();
+        assert_eq!(v2, 5);
+
+        let v3 = handle.get().await.unwrap();
+        assert_eq!(v3, 5);
+
+        let v4 = handle2.add(-1).await.unwrap();   
+        assert_eq!(v4, 4);
+
+        let v5 = handle2.get().await.unwrap();
+        assert_eq!(v5, 4);
+
+        let _ = handle.stop().await;
+    }
+}
